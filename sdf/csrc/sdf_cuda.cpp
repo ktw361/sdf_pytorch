e8a8a6e74af06a02643531f6bb7affab9c1d6e04
Zhifan <ktw361@gmail.com>
2023-10-19 23:32:22 +0100

    Update sdf_cuda.cpp

--- sdf/csrc/sdf_cuda.cpp
+++ sdf/csrc/sdf_cuda.cpp
@@ -1,6 +1,7 @@
 #include <torch/torch.h>
+#include <c10/macros/Macros.h>
 
-#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
